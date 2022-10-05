fd07eae06ee71770a920e7ed431bf16e0389fba4
ChristianReino <85199701+ChristianReino@users.noreply.github.com>
2022-10-05 17:56:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-var SpeechRecognition = window.webkitSpeechRecognition
-
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
 var recognition = new SpeechRecognition();
 
 function start()
 {
-    document.getElementById("textbox").innerHTML = "";
+    document.getElementById("textbox").innerHTML = ""; 
     recognition.start();
-}
+} 
+ 
+recognition.onresult = function(event) {
 
-recognition.onresult = function(event)
-{
-    console.log(event);
+ console.log(event); 
 
-    var Content = event.results[0][0].transcript;
-    console.log(Content);
-    
+var Content = event.results[0][0].transcript;
+
     document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
 }
 
-    function speak()
-    {
-        var synth = windows.speechSynthesis;
 
-        speak_data = document.getElementById("textbox").value;
+function speak(){
+    var synth = window.speechSynthesis;
 
-        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    speak_data = "Taking you Selfie in 5 seconds";
 
-        synth.speak("utterThis")
-    }
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+  width:360,
+  height:250,
+  image_format : 'png',
+  png_quality:90
+});
+camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+  webcam.snap(function(data_url) {
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+  });
+}
